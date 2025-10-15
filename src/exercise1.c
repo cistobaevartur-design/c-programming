b7840f47519c341c00068545398e12f6e8256995
Артур Чистобаев <chistoart.23@gmail.com>
2025-10-15 13:56:09 +0700

    1

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    float array[10];
-    float sum = 0.0;
+    double array[10];
+    float sum = 0;
 
-    // Ввод данных
+    
     for (int i = 0; i < 10; ++i) {
-        scanf("%f", &array[i]);
+        scanf("%lf", &array[i]);
         sum += array[i];
     }
 
-    // Вычисление среднего значения и округление
+    
     float average = sum / 10;
-    average = roundf(average * 100) / 100;
 
-    // Вывод результата
-    printf("%.2f\n", average);
+    
+    printf("%f\n", average);
     
     return 0;
 }
