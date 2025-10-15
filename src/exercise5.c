34004f13406070977086661ae524d93f851f64eb
Артур Чистобаев <chistoart.23@gmail.com>
2025-10-15 14:10:33 +0700

    5

--- src/exercise5.c
+++ src/exercise5.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main() {
+    int A[10][3], B[3][10], C[10][10];
+
+    for (int i = 0; i < 10; ++i)
+        for (int j = 0; j < 3; ++j)
+            scanf("%d", &A[i][j]);
+
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 10; ++j)
+            scanf("%d", &B[i][j]);
+
+    for (int i = 0; i < 10; ++i)
+        for (int j = 0; j < 10; ++j) {
+            C[i][j] = 0;
+            for (int k = 0; k < 3; ++k)
+                C[i][j] += A[i][k] * B[k][j];
+        }
+
+    for (int i = 0; i < 10; ++i)
+        for (int j = 0; j < 10; ++j)
+            printf("%d ", C[i][j]);
+
+    printf("\n");
     return 0;
 }
