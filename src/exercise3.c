ffbe73c9c185d16d963b816a7c2118a231adef12
Артур Чистобаев <chistoart.23@gmail.com>
2025-10-15 14:05:34 +0700

    3

--- src/exercise3.c
+++ src/exercise3.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    printf("Hello World!");
+    float array[10], result[10];
+    int K;
+
+
+    for (int i = 0; i < 10; ++i) {
+        scanf("%f", &array[i]);
+    }
+    scanf("%d", &K);
+
+    K %= 10; 
+    if (K < 0)
+        K += 10;
+    for (int i = 0; i < 10; ++i) {
+        result[(i + K) % 10] = array[i];
+    }
+
+    
+    for (int i = 0; i < 10; ++i) {
+        printf("%.f ", result[i]);
+    }
+    printf("\n");
+
     return 0;
 }
