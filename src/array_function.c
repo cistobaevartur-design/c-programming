c6c4b95858f2d3d3e0ac026863b778d95b23cd39
Чистобаев Артур Александрович и ИП-516 <chistoart.23@gmail.com>
2025-10-29 11:48:03 +0700

    1

--- src/array_function.c
+++ src/array_function.c
@@ -1,8 +1,58 @@
 #include "../include/array_function.h"
 
-void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {}
+void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {
+    for (int i =0; i < size_src1; i++){
 
-void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {}
+    
+        c[i] = a[i]}
+    for (int i = 0; i < size_src2; i++){
+        c[5 +i] = b[i]
+    }
+}
 
-void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {}
+#include "../include/array_function.h"
 
+void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
+    size_t i = 0, j = 0, k = 0;
+    
+    while (i < size_src1 && j < size_src2) {
+        if (src1[i] <= src2[j]) {
+            dest[k] = src1[i];
+            i++;
+        } else {
+            dest[k] = src2[j];
+            j++;
+        }
+        k++;
+    }
+    
+    while (i < size_src1) {
+        dest[k] = src1[i];
+        i++;
+        k++;
+    }
+    
+    while (j < size_src2) {
+        dest[k] = src2[j];
+        j++;
+        k++;
+    }
+}
+
+void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
+    if (size == 0) {
+        return;
+    }
+    
+    *min = src[0];
+    *max = src[0];
+    
+    for (size_t i = 1; i < size; i++) {
+        if (src[i] < *min) {
+            *min = src[i];
+        }
+        if (src[i] > *max) {
+            *max = src[i];
+        }
+    }
+}
