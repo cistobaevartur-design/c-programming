41db593129f65b3d37283137c136726cbc6eb476
cistobaevartur-design <cistobaevartur@gmail.com>
2025-11-05 16:18:57 +0700

    12

--- src/array_function.c
+++ src/array_function.c
@@ -1,42 +1,39 @@
-#include "../include/array_function.h"
 #include <stdio.h>
 
-void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {
-    for (int i =0; i < size_src1; i++){
 
-    
-        c[i] = a[i]}
-    for (int i = 0; i < size_src2; i++){
-        c[5 +i] = b[i]
+void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {
+    for (size_t i = 0; i < size_src1; i++) {
+        dest[i] = src1[i];
+    }
+    for (size_t i = 0; i < size_src2; i++) {
+        dest[size_src1 + i] = src2[i];
     }
 }
 
-
-
 void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
-    size_t i = 0, j = 0, k = 0;
-    
-    while (i < size_src1 && j < size_src2) {
-        if (src1[i] <= src2[j]) {
-            dest[k] = src1[i];
-            i++;
-        } else {
-            dest[k] = src2[j];
-            j++;
+    for (size_t i = 0; i < size_src1; ++i) {
+        dest[i] = src1[i];
+    }
+
+    for (size_t i = 0; i < size_src2; ++i) {
+        dest[i + size_src1] = src2[i];
+    }
+
+    size_t totalsize = size_src1 + size_src2;
+
+    for (size_t i = 0; i < totalsize; ++i) {
+        size_t min_i = i;
+        for (size_t j = i + 1; j < totalsize; ++j) {
+            if (dest[j] < dest[min_i]) {
+                min_i = j;
+            }
         }
-        k++;
-    }
-    
-    while (i < size_src1) {
-        dest[k] = src1[i];
-        i++;
-        k++;
-    }
-    
-    while (j < size_src2) {
-        dest[k] = src2[j];
-        j++;
-        k++;
+        
+        if (min_i != i) {
+            int temp = dest[i];
+            dest[i] = dest[min_i];
+            dest[min_i] = temp;
+        }
     }
 }
 
